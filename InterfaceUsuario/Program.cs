9e7403ae85fecb65e2c83484f6a2491a25d7f632
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-04 18:08:38 -0300

    Add project files.

---
+++ InterfaceUsuario/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace InterfaceUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GeradorDeInterface gerador = new GeradorDeInterface();
+            gerador.Gerar();
+        }
+    }
+}
